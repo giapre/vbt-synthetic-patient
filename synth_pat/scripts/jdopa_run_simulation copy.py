2ca7ad61e9f71758d4809228d967c115bc3f69ed
giapre <g.preti98@gmail.com>
2026-02-23 19:03:40 +0100

    added snakemake

--- synth_pat/scripts/jdopa_run_simulation copy.py
+++ synth_pat/scripts/jdopa_run_simulation copy.py
@@ -29,7 +29,7 @@ DATA_DIR = Paths.DATA
 
 type_of_sweep = 'jdopa_ws'
 
-ws = 10**np.linspace(-3,0,10)
+ws_array = 10**np.linspace(-3,0,10)
 njdopa_ctx_arr = np.round(10**np.linspace(-3,2,10),3)
 njdopa_str_arr = np.round(10**np.linspace(-3,2,10),3)
 
@@ -62,7 +62,7 @@ setup = {
     "params": gm.sigm_d1d2sero_default_theta,
     "v_c": 3.9,
     "horizon": 650,
-    "num_item": ws.shape[0],
+    "num_item": 1,
     "dt": 0.1,
     "num_skip": 10,
     "num_time": 300000,
@@ -87,52 +87,53 @@ Rd1, Rd2, Rsero = setup_receptors()
 # Run sweep
 # ------------------------
 
-for njdopa_ctx in njdopa_ctx_arr:
-    for njdopa_str in njdopa_str_arr:
-        JJdopa = np.ones(len(regions_names)) * njdopa_ctx
-        for region in ['L.PU', 'R.PU', 'L.CA', 'R.CA', 'L.PA', 'R.PA', 'L.AC', 'R.AC']:
-            idx = regions_names.index(region)
-            JJdopa[idx] = njdopa_str
+for ws in ws_array:
+    for njdopa_ctx in njdopa_ctx_arr:
+        for njdopa_str in njdopa_str_arr:
+            JJdopa = np.ones(len(regions_names)) * njdopa_ctx
+            for region in ['L.PU', 'R.PU', 'L.CA', 'R.CA', 'L.PA', 'R.PA', 'L.AC', 'R.AC']:
+                idx = regions_names.index(region)
+                JJdopa[idx] = njdopa_str
 
-        theta = gm.sigm_d1d2sero_default_theta._replace(
-            I=46.5,
-            Ja=Ja,
-            Jsa=Ja,
-            Jsg=13,
-            Jg=0,
-            Jdopa=JJdopa,
-            Rd1=Rd1,
-            Rd2=Rd2,
-            Rs=Rsero,
-            Sd1=-10.0,
-            Sd2=-10.0,
-            Ss=-40.0,
-            Zd1=0.5,
-            Zd2=1.0,
-            Zs=0.25,
-            we=0.25,
-            wi=1.,
-            wd=1,
-            ws=ws,
-            sigma_V=setup["noise"],
-            sigma_u=0.1 * setup["noise"],
-        )
+            theta = gm.sigm_d1d2sero_default_theta._replace(
+                I=46.5,
+                Ja=Ja,
+                Jsa=Ja,
+                Jsg=13,
+                Jg=0,
+                Jdopa=JJdopa,
+                Rd1=Rd1,
+                Rd2=Rd2,
+                Rs=Rsero,
+                Sd1=-10.0,
+                Sd2=-10.0,
+                Ss=-40.0,
+                Zd1=0.5,
+                Zd2=1.0,
+                Zs=0.25,
+                we=0.25,
+                wi=1.,
+                wd=1,
+                ws=ws,
+                sigma_V=setup["noise"],
+                sigma_u=0.1 * setup["noise"],
+            )
 
-        setup["params"] = theta
+            setup["params"] = theta
 
-        tic = time.time()
-        print(f"Running simulations for njdopa_ctx {njdopa_ctx} and {njdopa_str}")
-        bold, raw = run_bold_sweep((theta, setup))
-        toc = time.time()
+            tic = time.time()
+            print(f"Running simulations for njdopa_ctx {njdopa_ctx} and {njdopa_str}")
+            bold, raw = run_bold_sweep((theta, setup))
+            toc = time.time()
 
-        bold = np.asarray(bold)
-        raw = np.asarray(raw)
+            bold = np.asarray(bold)
+            raw = np.asarray(raw)
 
-        output_file = os.path.join(
-            RESULTS_DIR,
-            f"{type_of_sweep}_sweep/njdopa_ctx={njdopa_ctx}_njdopa_str={njdopa_str}"
-        )
+            output_file = os.path.join(
+                RESULTS_DIR,
+                f"{type_of_sweep}_sweep/ws={ws}_njdopa_ctx={njdopa_ctx}_njdopa_str={njdopa_str}"
+            )
 
-        np.savez(output_file, bold=bold, raw=raw)
+            np.savez(output_file, bold=bold, raw=raw)
 
-        print(f"Finished in {toc - tic} seconds")
+            print(f"Finished in {toc - tic} seconds")
